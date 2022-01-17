f5ed4d00e2d02c2fb1afe59a8c01bbd3cd3fc251
Khanjan Parikh <developerkhanju09@gmail.com>
2022-01-17 14:28:51 +0530

    tictactoe

---
+++ tic_tac_toe.py
@@ -0,0 +1,72 @@
+from sqlite3 import Row
+
+
+board = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+]
+
+def print_board(board):
+    for row in board:
+        for slot in row:
+            print(f"{slot}", end="")
+        print()
+
+print_board(board)
+
+def quit(user_input):
+    if user_input.lower() == 'q': 
+        print("Thanks for Playing!!")
+        return True
+    else: return False
+
+def check_input(user_input):
+    # check if its a number
+    if not isnum(user_input): return False
+    user_input = int(user_input)
+    # check if its 1-9
+    if not bounds(user_input): return False
+
+    return True
+
+def isnum(user_input):
+    if not user_input.isnumeric():
+        print("This is not a valid number")
+        return False
+    else: return True
+
+def bounds(user_input):
+    if user_input > 9 or user_input < 1:
+        print("This number is out of bounds!")
+        return False
+    else: return True
+
+def istaken(coords, board):
+    row = coords[0]
+    col = coords[1]
+    if board[row][col] != "-":
+        print("This position already taken")
+        return True
+    else:
+        return False
+
+def coordinates(user_input):
+    row = int(user_input / 3)
+    col = user_input
+    if col > 2: col = int(col % 3)
+    return (row, col)
+
+while True:
+    print_board(board)
+    user_input = input("Enter a position 1 through 9 or enter \"q\" to quit: ")
+    if quit(user_input): break
+    if not check_input(user_input):
+        print("Please try again.")
+        continue
+    user_input = int(user_input) - 1
+    coords = coordinates(user_input)
+    board[0][0] = "x"
+    if istaken(coords, board):
+        print("Please try again!")
+        continue 
