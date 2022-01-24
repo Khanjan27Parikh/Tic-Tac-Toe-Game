ae4115c7884b39e326b4852e534c611fad3f7f66
Khanjan Parikh <developerkhanju09@gmail.com>
2022-01-24 09:03:55 +0530

    Final

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -7,10 +7,13 @@ board = [
     ["-", "-", "-"]
 ]
 
+user = True  #When true it refers to x, otherwise o
+turns = 0
+
 def print_board(board):
     for row in board:
         for slot in row:
-            print(f"{slot}", end="")
+            print(f"{slot}", end=" ")
         print()
 
 print_board(board)
@@ -57,7 +60,51 @@ def coordinates(user_input):
     if col > 2: col = int(col % 3)
     return (row, col)
 
-while True:
+def add_to_board(coords, board, active_user):
+    row = coords[0]
+    col = coords[1]
+    board[row][col] = active_user
+
+def current_user(user):
+    if user: return "x"
+    else: return "o"
+
+def iswin(user, board):
+    if check_row(user, board): return True
+    if check_col(user, board): return True
+    if check_diag(user, board): return True
+    return False
+
+def check_row(user, board):
+    for row in board:
+        complete_row = True
+        for slot in row:
+            if slot != user:
+                complete_row = False
+                break
+        if complete_row: return True
+    return False
+
+def check_col(user, board):
+    for col in range(3):
+        complete_col = True
+        for row in range(3):
+            if board[row][col] != user:
+                complete_col = False
+                break
+        if complete_col: return True
+    return False
+
+def check_diag(user, board):
+    # top left to bottom right
+    if board[0][0] == user and board[1][1] == user and board[2][2] == user:
+        return True
+    elif board[0][2] == user and board[1][1] == user and board[2][0] == user:
+        return True
+    else: return False
+
+while turns < 9:
+    active_user = current_user(user)
     print_board(board)
     user_input = input("Enter a position 1 through 9 or enter \"q\" to quit: ")
     if quit(user_input): break
@@ -66,7 +113,14 @@ while True:
         continue
     user_input = int(user_input) - 1
     coords = coordinates(user_input)
-    board[0][0] = "x"
     if istaken(coords, board):
         print("Please try again!")
         continue 
+    add_to_board(coords, board, active_user)
+    if iswin(active_user, board):
+        print(f"{active_user.upper()} won!")
+        break
+
+    turns += 1
+    if turns == 9: print("Tie!")
+    user = not user
